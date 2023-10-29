7bb43155368fb2c0a1fa97c66079c487584f92e9
Tulanics <tulanics@gmail.com>
2023-10-29 11:07:56 -0300

    iniciando repositório git com projeo já iniciado

---
+++ BudgetAPI/BudgetAPI/Controllers/BudgetIncomeController.cs
@@ -0,0 +1,22 @@
+﻿using BudgetAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BudgetAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BudgetIncomeController : ControllerBase
+{
+    private static List<Income> incomes = new List<Income>();
+
+    [HttpPost]
+    public void AddsBudgetIncome([FromBody]Income income)
+    {
+        incomes.Add(income);
+        Console.WriteLine(income.Id);
+        Console.WriteLine(income.Description);
+        Console.WriteLine(income.Amount);
+        Console.WriteLine(income.Date);
+    }
+}
---
+++ BudgetAPI/BudgetAPI/Models/Income.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BudgetAPI.Models
+{
+    public class Income
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "A Descrição da receita é obrigatória")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "O valor da receita é obrigatório")]
+        [Range(0, double.MaxValue, ErrorMessage = "o valor da receita não pode ser negativo")]
+        public double Amount { get; set; }
+        
+        [Required]
+        
+        public DateTime Date { get; set; }
+    }
+}
